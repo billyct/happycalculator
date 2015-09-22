d6fba494485c2d48d7975289b85b112163a34345
billyct <billy_allen@126.com>
2015-09-23 07:13:35 +0800

    add chaijs to test and some other fix

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var assert = require('assert');
-var calculator = require('../libs/calculator');
+var expect = require('chai').expect;
+var happycalculator = require('../libs/calculator');
 
 
 describe('calculator', function () {
@@ -8,23 +8,23 @@ describe('calculator', function () {
   describe('fixBracketsPre', function () {
     it("(20) should return (, 20)", function () {
       var test_data = '(20)';
-      assert.deepEqual(['(', '20)'], calculator.fixBracketsPre(test_data));
+      expect(happycalculator.fixBracketsPre(test_data)).to.deep.equal(['(', '20)']);
     });
   });
 
   describe('fixBracketsPost', function () {
     it("(20) should return (20)", function () {
       var test_data = '(20)';
-      assert.deepEqual(['(20)'], calculator.fixBracketsPost(test_data));
+      expect(happycalculator.fixBracketsPost(test_data)).to.deep.equal(['(20)']);
     });
     it("20) should return 20, )", function () {
       var test_data = '20)';
-      assert.deepEqual(['20', ')'], calculator.fixBracketsPost(test_data));
+      expect(happycalculator.fixBracketsPost(test_data)).to.deep.equal(['20', ')']);
     });
 
     it("20)) should return 20, ), )", function () {
       var test_data = '20))';
-      assert.deepEqual(['20', ')', ')'], calculator.fixBracketsPost(test_data));
+      expect(happycalculator.fixBracketsPost(test_data)).to.deep.equal(['20', ')', ')']);
     });
   });
 
@@ -32,7 +32,7 @@ describe('calculator', function () {
   describe('fixBrackets', function () {
     it("(20) should return 20", function () {
       var test_data = '(20)';
-      assert.deepEqual(['20'], calculator.fixBrackets(test_data));
+      expect(happycalculator.fixBrackets(test_data)).to.deep.equal(['20']);
     });
   });
 
@@ -42,39 +42,43 @@ describe('calculator', function () {
 
 
     afterEach(function() {
-      calculator.removeFormulas();
+      happycalculator.removeFormulas();
     });
 
 
     it("sqrt(20) should return ['(', '20', '*', '20', ')']", function() {
       var test_data = 'sqrt(20)';
-      assert.deepEqual(['(', '20', '*', '20', ')'], calculator.fixFormulas(test_data));
+      expect(happycalculator.fixFormulas(test_data)).to.deep.equal(['(', '20', '*', '20', ')']);
     });
 
     it("sqrt(20 + 20) should return ['(', '20', '*', '20', ')']", function() {
       var test_data = 'sqrt(20+20)';
-      assert.deepEqual(['(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')'], calculator.fixFormulas(test_data));
+      expect(happycalculator.fixFormulas(test_data)).to.deep.equal(['(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')']);
+
     });
 
     it("sqrt(sqrt(20)) should return ['(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')']", function() {
       var test_data = 'sqrt(sqrt(20))';
-      assert.deepEqual(['(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')'], calculator.fixFormulas(test_data));
+      expect(happycalculator.fixFormulas(test_data)).to.deep.equal(['(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')']);
+
     });
 
     it("sqrt(20) should return ['(', '20', '+', '20', ')']", function() {
-      calculator.addFormulas({
+      happycalculator.addFormulas({
         'sqrt': '$1 + $1'
       });
       var test_data = 'sqrt(20)';
-      assert.deepEqual(['(', '20', '+', '20', ')'], calculator.fixFormulas(test_data));
+      expect(happycalculator.fixFormulas(test_data)).to.deep.equal(['(', '20', '+', '20', ')']);
+
     });
 
     it("custom(20, 10) should return ['(', '20', '+', '10', ')']", function() {
-      calculator.addFormulas({
+      happycalculator.addFormulas({
         'custom': '$1 + $2'
       });
       var test_data = 'custom(20, 10)';
-      assert.deepEqual(['(', '20', '+', '10', ')'], calculator.fixFormulas(test_data));
+      expect(happycalculator.fixFormulas(test_data)).to.deep.equal(['(', '20', '+', '10', ')']);
+
     });
 
 
@@ -83,20 +87,22 @@ describe('calculator', function () {
   describe('fixFormulasLoop', function() {
     it("[ 'sqrt(sqrt(2', ')', ')' ] should return ['sqrt(sqrt(2))']", function() {
       var test_data = [ 'sqrt(sqrt(2', ')', ')' ];
-      assert.deepEqual(['sqrt(sqrt(2))'], calculator.fixFormulasLoop(test_data));
+      expect(happycalculator.fixFormulasLoop(test_data)).to.deep.equal(['sqrt(sqrt(2))']);
+
     });
     it("[ 'sqrt(2', '+', '2', ')' ] should return ['sqrt(2+2)']", function() {
       var test_data = [ 'sqrt(2', '+', '2', ')' ];
-      assert.deepEqual(['sqrt(2+2)'], calculator.fixFormulasLoop(test_data));
+      expect(happycalculator.fixFormulasLoop(test_data)).to.deep.equal(['sqrt(2+2)']);
+
     });
     it("[ 'sqrt(sqrt(2', '+', '2', ')', ')' ] should return ['sqrt(sqrt(2+2))']", function() {
       var test_data = [ 'sqrt(sqrt(2', '+', '2', ')', ')' ];
-      assert.deepEqual(['sqrt(sqrt(2+2))'], calculator.fixFormulasLoop(test_data));
+      expect(happycalculator.fixFormulasLoop(test_data)).to.deep.equal(['sqrt(sqrt(2+2))']);
     });
 
     it("[ 'sqrt(sqrt(2', '+', '2', ')', ')' ] should return ['sqrt(sqrt(sqrt(2+2)))']", function() {
       var test_data = [ 'sqrt(sqrt(sqrt(2', '+', '2', ')', ')', ')' ];
-      assert.deepEqual(['sqrt(sqrt(sqrt(2+2)))'], calculator.fixFormulasLoop(test_data));
+      expect(happycalculator.fixFormulasLoop(test_data)).to.deep.equal(['sqrt(sqrt(sqrt(2+2)))']);
     });
   });
 
@@ -104,30 +110,35 @@ describe('calculator', function () {
   describe('convert', function () {
     it("20 * ( 10 + 20 ) / 20 should return ['20', '*', '(', '10', '+', '20', ')', '/', '20']", function () {
       var test_data = '20 * ( 10 + 20 ) / 20';
-      assert.deepEqual(['20', '*', '(', '10', '+', '20', ')', '/', '20'], calculator.convert(test_data));
+      expect(happycalculator.convert(test_data)).to.deep.equal(['20', '*', '(', '10', '+', '20', ')', '/', '20']);
+
     });
 
     it("20 * ( 10 + 20 ) / sqrt(20) should return ['20', '*', '(', '10', '+', '20', ')', '/', '(', '20', '*', '20', ')']", function() {
       var test_data = '20 * ( 10 + 20 ) / sqrt(20)';
-      assert.deepEqual(['20', '*', '(', '10', '+', '20', ')', '/', '(', '20', '*', '20', ')'], calculator.convert(test_data));
+      expect(happycalculator.convert(test_data)).to.deep.equal(['20', '*', '(', '10', '+', '20', ')', '/', '(', '20', '*', '20', ')']);
+
     });
 
 
     it("20 * ( 10 + 20 ) / sqrt(20 + 20) should return ['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')']", function() {
       var test_data = '20 * ( 10 + 20 ) / sqrt(20 + 20)';
-      assert.deepEqual(['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')'], calculator.convert(test_data));
+      expect(happycalculator.convert(test_data)).to.deep.equal(['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')']);
+
     });
 
 
     it("20 * ( 10 + 20 ) / sqrt(sqrt(20)) should return ['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')']", function() {
       var test_data = '20 * ( 10 + 20 ) / sqrt(sqrt(20))';
-      assert.deepEqual(['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')'], calculator.convert(test_data));
+      expect(happycalculator.convert(test_data)).to.deep.equal(['20', '*', '(', '10', '+', '20', ')', '/', '(', '(', '20', '*', '20', ')', '*', '(', '20', '*', '20', ')', ')']);
+
     });
 
     it("sqrt(sqrt(20+20)) should return right", function() {
       var test_data = 'sqrt(sqrt(20+20))';
+      expect(happycalculator.convert(test_data)).to.deep.equal(['(', '(', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')', ')', '*', '(', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')', ')', ')']);
+
       //((((20+20)*(20+20)))*(((20+20)*(20+20))))
-      assert.deepEqual(['(', '(', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')', ')', '*', '(', '(', '(', '20', '+', '20', ')', '*', '(', '20', '+', '20', ')', ')', ')', ')'], calculator.convert(test_data));
     });
   });
 
@@ -135,41 +146,46 @@ describe('calculator', function () {
 
     it("20 * ( 10 + 20 ) / 20 should return [ '20', '10', '20', '+', '*', '20', '/' ]", function() {
       var test_data = '20 * ( 10 + 20 ) / 20';
-      assert.deepEqual([ '20', '10', '20', '+', '*', '20', '/' ], calculator.shunt(test_data));
+
+      expect(happycalculator.shunt(test_data)).to.deep.equal([ '20', '10', '20', '+', '*', '20', '/' ]);
+
     });
   });
 
   describe('calculate', function() {
     it("20 * ( 10 + 20 ) / 20 should return 30", function() {
       var test_data = '20 * ( 10 + 20 ) / 20';
-      assert.equal(30, calculator.calculate(test_data));
+      expect(happycalculator.calculate(test_data)).to.equal(30);
+
     });
 
     it("sqrt(sqrt(20)) should return 160000", function() {
       var test_data = 'sqrt(sqrt(20))';
-      assert.equal(160000, calculator.calculate(test_data));
+      expect(happycalculator.calculate(test_data)).to.equal(160000);
     });
 
 
     it("sqrt(20+20) should return 1600", function() {
       var test_data = 'sqrt(20+20)';
-      assert.equal(1600, calculator.calculate(test_data));
+      expect(happycalculator.calculate(test_data)).to.equal(1600);
     });
 
 
     it("sqrt(20+20) + sqrt(20+20) should return 3200", function() {
       var test_data = 'sqrt(20+20) + sqrt(20+20)';
-      assert.equal(3200, calculator.calculate(test_data));
+      expect(happycalculator.calculate(test_data)).to.equal(3200);
+
     });
 
     it("sqrt(sqrt(2+2)) should return 256", function() {
       var test_data = 'sqrt(sqrt(2+2))';
-      assert.equal(256, calculator.calculate(test_data));
+      expect(happycalculator.calculate(test_data)).to.equal(256);
+
     });
 
     it("custom(sqrt(2+2)) should throw error", function() {
       var test_data = 'custom(2+2)';
-      assert.throws(function(){calculator.calculate(test_data);}, Error, 'unvalid expression');
+      expect(function(){happycalculator.calculate(test_data);}).to.throw(Error, 'unvalid expression');
     });
   });
 
@@ -178,12 +194,14 @@ describe('calculator', function () {
 
     it("a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab should return ['(3+4)*3']", function() {
       var test_data = 'a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab';
-      assert.deepEqual(['(3+4)*3'], calculator.parse(test_data));
+      expect(happycalculator.parse(test_data)).to.deep.equal(['(3+4)*3']);
+
     });
 
     it("a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab; ab = 4; ab+ab should return ['(3+4)*3', '4+4']", function() {
       var test_data = 'a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab; ab = 4; ab+ab';
-      assert.deepEqual(['(3+4)*3', '4+4'], calculator.parse(test_data));
+      expect(happycalculator.parse(test_data)).to.deep.equal(['(3+4)*3', '4+4']);
+
     });
 
   });
@@ -193,16 +211,14 @@ describe('calculator', function () {
 
     it("a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab should return [21]", function() {
       var test_data = 'a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab';
-      assert.deepEqual([21], calculator.calculateCode(test_data));
+      expect(happycalculator.calculateCode(test_data)).to.deep.equal([21]);
     });
 
     it("a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab; ab = 4; ab+ab should return [21, 8]", function() {
       var test_data = 'a=$1 + $2; ab = 3; abc = 4; a(ab,abc)*ab; ab = 4; ab+ab';
-      assert.deepEqual([21, 8], calculator.calculateCode(test_data));
+      expect(happycalculator.calculateCode(test_data)).to.deep.equal([21, 8]);
     });
 
   });
-
-
 
 });
